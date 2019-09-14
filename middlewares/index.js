f6ccb3b79f40e9e5e168fa4a74d14838e0690852
A๑take <morethansean@gmail.com>
2019-09-14 19:45:28 +0800

    fix: sessions cannot start with '_'

--- middlewares/index.js
+++ middlewares/index.js
@@ -11,7 +11,7 @@ exports.auth = (req, res, next) => {
   }
   
   // extend the session expiration every day
-  req.session._days = Math.floor(Date.now() / (60*60*24e3));
+  req.session.days = Math.floor(Date.now() / (60*60*24e3));
 
   req.userRequest = userRequest.get('default');
   return next();
