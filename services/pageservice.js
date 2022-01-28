269bf2ef1f043594d7527f0d97eb2b141ee0df48
A๑take <morethansean@gmail.com>
2022-01-28 13:04:15 +0800

    feat: add userAgent to requests

--- services/pageservice.js
+++ services/pageservice.js
@@ -41,7 +41,7 @@ exports.PAGES = {
   NEVERLAND: 'NEVERLAND'
 };
 
-exports.fetchPage = function fetchPage(request = requestWithoutCookie, url, userAgent) {
+exports.fetchPage = function fetchPage(request = requestWithoutCookie, url, userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36') {
   console.log('fetching url:', url);
   let options = {};
   if (userAgent) {
